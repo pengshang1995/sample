8194e18b79855941c8b127d3de9a4f618a455637
PS <786188095@qq.com>
2018-02-27 03:04:54 +0000

    完成路由和前端界面优化

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,8 +2,13 @@
 <html>
 <head>
     <title>@yield('title', 'Sample')</title>
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-   @yield('content')
+    @include('layouts._header')
+    <div class="container">
+        @yield('content')
+    </div>
+    @include('layouts._footer')
 </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.default')
 @section('title','主页')
 @section('content')
-    <h1>主页</h1>
+    <div class="jumbotron">
+        <h1>hello Laravel</h1>
+        <p class="lead">
+            你现在看到得是 <a href="http://blog.psonlyweb.cn">彭尚的博客</a>
+        </p>
+        <p>一切从这里开始</p>
+        <p>
+            <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
+        </p>
+    </div>
+
 @stop
